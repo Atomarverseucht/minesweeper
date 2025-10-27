707e10b7fc02d58f7206c05e91bcf7f5971dcee1
Guakocius <engelhardt.alexander@mein.gmx>
2025-10-27 21:47:36 +0100

    Added 80% test coverage for Board.scala

--- src/test/scala/TestInitializeBoard.scala
+++ src/test/scala/TestInitializeBoard.scala
@@ -1,13 +1,22 @@
 package de.htwg
 
-import munit.FunSuite
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import de.htwg.Board._
 
-class TestInitializeBoard extends FunSuite {
-    test("initialized Board meets all requirements") {
-        val b = Board(3, 4, 12, 12, 10)
+class TestInitializeBoard extends AnyWordSpec with Matchers {
+    "The Board" should:
+        "be initialized" in:
+            val b = Board(3, 4, 12, 12, 10)
+            an [IllegalArgumentException] should be thrownBy Board(3, 4, 12, 12, 1000)
 
-        assertEquals((b.getSize), (12,12))
-        
-    }
-
+            b.getSize should be (12, 12)
+            b.bombCount should (be > 1 and be < maxBombs(b.xSize, b.ySize))
+            emojify(8) should not be empty
+            emojify(8) shouldBe "8️⃣"
+            b.inGame shouldBe b.checkGameState
+            b.getBombNeighbour(3, 4) shouldBe 0
+            b.openField(5, 4) should (equal('b') or equal('c') or equal('0') or
+                equal('1') or equal('2') or equal('3') or equal('4') or
+                equal('5') or equal('6') or equal('7') or equal('8'))
 }
