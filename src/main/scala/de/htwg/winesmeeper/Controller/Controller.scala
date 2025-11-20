92388ae5ad8b0062b2c954afa441150cc0db7f33
Atom <atomcraft.4u@web.de>
2025-11-20 20:19:09 +0100

    fix: Edge bug fixed

--- src/main/scala/de/htwg/winesmeeper/Controller/Controller.scala
+++ src/main/scala/de/htwg/winesmeeper/Controller/Controller.scala
@@ -50,7 +50,11 @@ object Controller:
   def initController(xSize: Int, ySize: Int, xStart: Int, yStart: Int, bombCount: Int): Controller =
     require(xSize >= 10 && ySize >= 10, "x and y size must be >= 10")
     require(xStart <= xSize && xStart >= 0 && yStart <= ySize && yStart >= 0, "Starting position must be on the field")
-    val bMax = Board.maxBombs(xSize, ySize)
+    val ex =
+      if isEdge(xSize, xStart) && isEdge(ySize,yStart) then 5
+      else if isEdge(xSize, xStart) || isEdge(ySize, yStart) then 3
+      else 0
+    val bMax: Int = Board.maxBombs(xSize, ySize) + ex
     require(bombCount >= 1 && bombCount <= bMax, s"Bomb Count must be between 1 and $bMax")
     val boardv = initField(0, 0, xStart, yStart, Vector.fill(xSize, ySize)(Field(false, true)), bombCount, bMax)
     new Controller(new Board(boardv, inGame = true))
@@ -60,13 +64,16 @@ object Controller:
     if fieldCount <= 0 then
       boardv
     else
-      val newV: (Boolean, Int, Field) = if Board.isNeighbour(xStart, yStart, indx, indy) then
-        (false, fieldCount, Field(false, true))
-      else
-        val isBomb = Random.nextInt(fieldCount) < bombCount
-        (isBomb, fieldCount - 1, Field(isBomb, false))
+      val newV: (Boolean, Int, Field) =
+        if Board.isNeighbour(xStart, yStart, indx, indy) then
+          (false, fieldCount, Field(false, true))
+        else
+          val isBomb = Random.nextInt(fieldCount) < bombCount
+          (isBomb, fieldCount - 1, Field(isBomb, false))
       
       val nboard = boardv.updated(indx, boardv(indx).updated(indy, newV._3))
       val nbc = if newV._1 then bombCount - 1 else bombCount
       val nextC = if indx + 1 < boardv.length then (indx + 1, indy) else (0, indy+1)
       initField(nextC._1, nextC._2, xStart, yStart, nboard, nbc, newV._2)
+
+  def isEdge(size: Int, x: Int): Boolean =  x == 0 || x == size - 1
--- src/main/scala/de/htwg/winesmeeper/aView/Main.scala
+++ src/main/scala/de/htwg/winesmeeper/aView/Main.scala
@@ -14,7 +14,6 @@ import scala.annotation.tailrec
 
 class View extends Observer:
   val ctrl: Controller = TUI.initController
-  print(ctrl.gb.board)
   ctrl.addSub(this)
   println(TUI.getBoardString(ctrl))
 
