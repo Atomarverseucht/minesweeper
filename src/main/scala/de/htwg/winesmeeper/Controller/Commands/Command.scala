ad1d13ec0a921a358a102ddf73477934692412c3
Atom <atomcraft.4u@web.de>
2025-11-30 02:24:50 +0100

    fix: Help is now better formatted

--- src/main/scala/de/htwg/winesmeeper/Controller/Commands/Command.scala
+++ src/main/scala/de/htwg/winesmeeper/Controller/Commands/Command.scala
@@ -8,16 +8,21 @@ trait Command:
   def doStep(): Boolean
   def undoStep(): Boolean
   def redoStep(): Boolean
+
   
 trait CommandCOR:
   val cmd: String
   val helpMsg: String
   val next: CommandCOR
   def buildCmd(cmd: String, x: Int, y: Int, ctrl: Controller): Try[Command]
+  def listCmds: List[CommandCOR] = this::next.listCmds
   
 object LastElemCmdCOR extends CommandCOR:
   override val next: CommandCOR = this
   override val cmd: String = ""
   override val helpMsg: String = ""
-
   override def buildCmd(cmd: String, x: Int, y: Int, ctrl: Controller): Try[Command] =  Failure(IllegalArgumentException())
+
+  override def listCmds: List[CommandCOR] = Nil
+
+
--- src/main/scala/de/htwg/winesmeeper/Controller/SysCommands/HelpCmd.scala
+++ src/main/scala/de/htwg/winesmeeper/Controller/SysCommands/HelpCmd.scala
@@ -1,6 +1,8 @@
 package de.htwg.winesmeeper.Controller.SysCommands
 
 import de.htwg.winesmeeper.Controller.Controller
+import de.htwg.winesmeeper.Controller.Commands
+import de.htwg.winesmeeper.Controller.Commands.CORFlag
 
 object HelpCmd extends SysCommandCOR:
   override val cmd: String = "help"
@@ -9,6 +11,8 @@ object HelpCmd extends SysCommandCOR:
 
   override def execute(ctrl: Controller): String =
    val sysCmds = HelpCmd.listCmds
-   val scString = (for cmd <- sysCmds yield s"${cmd.cmd}: ${cmd.helpMsg}").mkString("\n")
-   s"Commands without parameters:\n$scString"
+   val scString = (for cmd <- sysCmds yield s"  ${cmd.cmd}: ${cmd.helpMsg}").mkString("\n")
+   val cmds = CORFlag.listCmds
+   val cmdString = (for cmd <- cmds yield s"  ${cmd.cmd}: ${cmd.helpMsg}").mkString("\n")
+   s"Commands without parameters:\n$scString \n\nCommands with coordinates:\n$cmdString"
 
