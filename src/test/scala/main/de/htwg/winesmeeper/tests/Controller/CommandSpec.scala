647a279305cf34ca042250478ed05809c8d814e6
Atom <atomcraft.4u@web.de>
2025-11-30 03:15:03 +0100

    redo test fix 2

--- src/test/scala/main/de/htwg/winesmeeper/tests/Controller/CommandSpec.scala
+++ src/test/scala/main/de/htwg/winesmeeper/tests/Controller/CommandSpec.scala
@@ -12,5 +12,5 @@ class CommandSpec extends AnyWordSpec with Matchers:
   "The Command" should:
     val testCtrl = Controller(10, 10, 1, 1, 25)
     "throw Exceptions" in:
-      LastElemCmdCOR.buildCmd("dosn't matter", 5, 5, testCtrl) shouldBe Failure(IllegalArgumentException())
+      LastElemCmdCOR.buildCmd("dosn't matter", 5, 5, testCtrl) shouldBe Failure(IllegalArgumentException)
       LastElemSysCommand.execute(testCtrl) shouldBe "No such command"
