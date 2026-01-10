ea273a05a6bfab9f3339cfc9c2f5bbc9174eec70
Atom <atomcraft.4u@web.de>
2026-01-10 20:28:33 +0100

    test fix

--- src/main/scala/de/htwg/winesmeeper/Model/ImplField/Field.scala
+++ src/main/scala/de/htwg/winesmeeper/Model/ImplField/Field.scala
@@ -4,5 +4,5 @@ import de.htwg.winesmeeper.Model.FieldTrait
 
 case class Field (isBomb: Boolean,
                   isOpened: Boolean,
-                  isFlag: Boolean)
+                  isFlag: Boolean = false)
   extends FieldTrait
